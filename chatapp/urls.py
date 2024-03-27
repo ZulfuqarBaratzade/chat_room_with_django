c0e4b21bf8f1bbf0859fa6fc774945bb9dc104cc
zulfuqar.baratzade <zulfuqar.baratzade@gmail.com>
2024-03-27 19:12:47 +0400

    feat: create room and message model

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from chatapp.views import home
+from chatapp.views import home,checkview
 
 urlpatterns = [
     path('',home,name="home"),
+    path("<str:room>/",home,name="home"),
+    path("checkview",checkview,name="checkview")
 ]
